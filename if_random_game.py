521b41070c501890ce121e3d481fc2bac9e82c4f
natalizubareva <49855757+natalizubareva@users.noreply.github.com>
2019-04-22 16:41:56 +0300

    Add files via upload

---
+++ if_random_game.py
@@ -0,0 +1,9 @@
+z=random.randint(1,4)
+user=int(input ("Enter number from 1 to 4"))
+if z==user:
+    print ("Victory")
+elif user<z:
+    print ("Your number is smaller")
+elif user>z:
+    print ("Your number is bigger")
+    
